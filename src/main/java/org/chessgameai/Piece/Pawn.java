0b9f6aace0e9b3a45c2fa647a3d5d385167f2336
Reda Nezzar <nezzar.reda@gmail.com>
2017-12-25 18:25:08 -0500

    Pawn Creation

---
+++ src/main/java/org/chessgameai/Piece/Pawn.java
@@ -0,0 +1,33 @@
+package org.chessgameai.Piece;
+
+import org.chessgameai.board.Alliance;
+
+import java.util.Collection;
+
+/**
+ * Created by reda on 12/25/17.
+ */
+public class Pawn extends Piece {
+
+
+    public Pawn(final Alliance alliance,
+                final int[] piecePosition,
+                final boolean isFirstMove) {
+        super(PieceType.PAWN, alliance, piecePosition, isFirstMove);
+    }
+
+    public Pawn(final Alliance alliance,
+                final int[] piecePosition) {
+        super(PieceType.PAWN, alliance, piecePosition, true);
+    }
+
+    @Override
+    void initPosition() {
+
+    }
+
+    @Override
+    public Collection<int[]> legalMoves() {
+        return null;
+    }
+}
