8bd2f120dedf071c4fd2457876e90650d9e354b8
Reda Nezzar <nezzar.reda@gmail.com>
2017-12-25 18:48:10 -0500

    remove the abstract method initPosition and create a general initPositions that returns all possible first position of each piece

--- src/main/java/org/chessgameai/Piece/Piece.java
+++ src/main/java/org/chessgameai/Piece/Piece.java
@@ -2,7 +2,10 @@ package org.chessgameai.Piece;
 
 import org.chessgameai.board.Alliance;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by reda on 12/25/17.
@@ -36,6 +39,64 @@ public abstract class Piece {
 
     public boolean isFirstMove() { return this.isFirstMove; }
 
+    public Collection<int[]> initPosition(){
+        final List<int[]> initPositions = new ArrayList<>();
+        switch (this.pieceType) {
+            case PAWN:
+                for (int j = 0; j < 8; j++) {
+                    if (this.pieceAlliance == Alliance.WHITE) {
+                        initPositions.add(new int[]{1, j});
+                    } else {
+                        initPositions.add(new int[]{6, j});
+                    }
+                }
+                break;
+            case QUEEN:
+                if (this.pieceAlliance == Alliance.WHITE) {
+                    initPositions.add(new int[]{0, 3});
+                } else {
+                    initPositions.add(new int[]{7, 3});
+                }
+                break;
+            case KING:
+                if (this.pieceAlliance == Alliance.WHITE) {
+                    initPositions.add(new int[]{0, 4});
+                } else {
+                    initPositions.add(new int[]{7, 4});
+                }
+                break;
+            case BISHOP:
+                if (this.pieceAlliance == Alliance.WHITE) {
+                    initPositions.add(new int[]{0, 2});
+                    initPositions.add(new int[]{0, 5});
+                } else {
+                    initPositions.add(new int[]{7, 2});
+                    initPositions.add(new int[]{7, 5});
+                }
+                break;
+            case KNIGHT:
+                if (this.pieceAlliance == Alliance.WHITE) {
+                    initPositions.add(new int[]{0, 1});
+                    initPositions.add(new int[]{0, 6});
+                } else {
+                    initPositions.add(new int[]{7, 1});
+                    initPositions.add(new int[]{7, 6});
+                }
+                break;
+            case ROOK:
+                if (this.pieceAlliance == Alliance.WHITE) {
+                    initPositions.add(new int[]{0, 0});
+                    initPositions.add(new int[]{0, 6});
+                } else {
+                    initPositions.add(new int[]{7, 0});
+                    initPositions.add(new int[]{7, 6});
+                }
+                break;
+
+        }
+        return initPositions;
+    }
+
     @Override
     public String toString() {
         return "Piece{" +
@@ -58,5 +119,7 @@ public abstract class Piece {
                 && isFirstMove == oPiece.isFirstMove
                 && pieceType == oPiece.pieceType;
     }
+
+    public abstract Collection<int[]> legalMoves();
 
 }
