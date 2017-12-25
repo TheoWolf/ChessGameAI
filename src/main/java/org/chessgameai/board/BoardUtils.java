332ee2871768a0a91971f7a98d6f5fdc2a8cde14
Reda Nezzar <nezzar.reda@gmail.com>
2017-12-25 18:26:01 -0500

    BoardUtils Creation to store the chess game parameters

---
+++ src/main/java/org/chessgameai/board/BoardUtils.java
@@ -0,0 +1,14 @@
+package org.chessgameai.board;
+
+/**
+ * Created by reda on 12/25/17.
+ */
+public enum BoardUtils {
+
+    INSTANCE;
+
+    public static final int NUM_TILES_PER_ROW = 8;
+    public static final int NUM_TILES_PER_COLUMN = 8;
+    public static final int NUM_TILES = 64;
+    
+}
