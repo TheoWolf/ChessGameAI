d75cc7ba2ef43c88bd3451290da819371de22973
Reda Nezzar <nezzar.reda@gmail.com>
2017-12-26 16:29:51 -0500

    Implement knight legal moves

--- src/main/java/org/chessgameai/Piece/Knight.java
+++ src/main/java/org/chessgameai/Piece/Knight.java
@@ -1,8 +1,12 @@
 package org.chessgameai.Piece;
 
+import com.sun.tools.doclint.Entity;
 import org.chessgameai.board.Alliance;
+import org.chessgameai.board.BoardUtils;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by reda on 12/25/17.
@@ -21,7 +25,88 @@ public class Knight extends Piece {
 
     @Override
     public Collection<int[]> legalMoves() {
-        //TODO
-        return null;
+        List<int[]> legalMoves =  new ArrayList<>();
+        int currentY = this.piecePosition[0];
+        int currentX = this.piecePosition[1];
+        int DIRECTION = this.pieceAlliance.getDirection();//Direction
+        int OPPOSITE_DIRECTION = this.pieceAlliance.getOppositeDirection();//Opposite Direction
+        if(currentX+2 < BoardUtils.NUM_TILES_PER_ROW
+                && currentX-2 >= 0
+                && currentY+2 < BoardUtils.NUM_TILES_PER_COLUMN
+                && currentY-2 >= 0){
+            legalMoves.add(new int[]{currentY+2, currentX+1});
+            legalMoves.add(new int[]{currentY+2, currentX-1});
+            legalMoves.add(new int[]{currentY-2, currentX+1});
+            legalMoves.add(new int[]{currentY-2, currentX-1});
+            legalMoves.add(new int[]{currentY+1, currentX+2});
+            legalMoves.add(new int[]{currentY+1, currentX-2});
+            legalMoves.add(new int[]{currentY-1, currentX+2});
+            legalMoves.add(new int[]{currentY-1, currentX-2});
+        }else if(currentX+2 < BoardUtils.NUM_TILES_PER_ROW
+                && currentX-2 >= 0
+                && currentY+2 < BoardUtils.NUM_TILES_PER_COLUMN){
+            if (currentY-1 >= 0) {
+                legalMoves.add(new int[]{currentY + 2, currentX + 1});
+                legalMoves.add(new int[]{currentY + 2, currentX - 1});
+                legalMoves.add(new int[]{currentY + 1, currentX + 2});
+                legalMoves.add(new int[]{currentY + 1, currentX - 2});
+                legalMoves.add(new int[]{currentY - 1, currentX + 2});
+                legalMoves.add(new int[]{currentY - 1, currentX - 2});
+            }else{
+                legalMoves.add(new int[]{currentY + 2, currentX + 1});
+                legalMoves.add(new int[]{currentY + 2, currentX - 1});
+                legalMoves.add(new int[]{currentY + 1, currentX + 2});
+                legalMoves.add(new int[]{currentY + 1, currentX - 2});
+            }
+        }else if(currentX+2 < BoardUtils.NUM_TILES_PER_ROW
+                && currentX-2 >= 0
+                && currentY-2 >= 0){
+            if (currentY+1 < BoardUtils.NUM_TILES_PER_COLUMN) {
+                legalMoves.add(new int[]{currentY - 2, currentX + 1});
+                legalMoves.add(new int[]{currentY - 2, currentX - 1});
+                legalMoves.add(new int[]{currentY + 1, currentX + 2});
+                legalMoves.add(new int[]{currentY + 1, currentX - 2});
+                legalMoves.add(new int[]{currentY - 1, currentX + 2});
+                legalMoves.add(new int[]{currentY - 1, currentX - 2});
+            }else{
+                legalMoves.add(new int[]{currentY - 2, currentX + 1});
+                legalMoves.add(new int[]{currentY - 2, currentX - 1});
+                legalMoves.add(new int[]{currentY - 1, currentX + 2});
+                legalMoves.add(new int[]{currentY - 1, currentX - 2});
+            }
+        }else if (currentX+2 < BoardUtils.NUM_TILES_PER_ROW
+                && currentY+2 < BoardUtils.NUM_TILES_PER_COLUMN
+                && currentY-2 >= 0){
+            if(currentX-1 >= 0) {
+                legalMoves.add(new int[]{currentY + 2, currentX + 1});
+                legalMoves.add(new int[]{currentY + 2, currentX - 1});
+                legalMoves.add(new int[]{currentY - 2, currentX + 1});
+                legalMoves.add(new int[]{currentY - 2, currentX - 1});
+                legalMoves.add(new int[]{currentY + 1, currentX + 2});
+                legalMoves.add(new int[]{currentY - 1, currentX + 2});
+            }else{
+                legalMoves.add(new int[]{currentY + 2, currentX + 1});
+                legalMoves.add(new int[]{currentY - 2, currentX + 1});
+                legalMoves.add(new int[]{currentY + 1, currentX + 2});
+                legalMoves.add(new int[]{currentY - 1, currentX + 2});
+            }
+        }else if (currentX-2 >= 0
+                && currentY+2 < BoardUtils.NUM_TILES_PER_COLUMN
+                && currentY-2 >= 0){
+            if(currentX+1 < BoardUtils.NUM_TILES_PER_ROW) {
+                legalMoves.add(new int[]{currentY + 2, currentX + 1});
+                legalMoves.add(new int[]{currentY + 2, currentX - 1});
+                legalMoves.add(new int[]{currentY - 2, currentX + 1});
+                legalMoves.add(new int[]{currentY - 2, currentX - 1});
+                legalMoves.add(new int[]{currentY + 1, currentX - 2});
+                legalMoves.add(new int[]{currentY - 1, currentX - 2});
+            }else{
+                legalMoves.add(new int[]{currentY + 2, currentX - 1});
+                legalMoves.add(new int[]{currentY - 2, currentX - 1});
+                legalMoves.add(new int[]{currentY + 1, currentX - 2});
+                legalMoves.add(new int[]{currentY - 1, currentX - 2});
+            }
+        }
+        return legalMoves;
     }
 }
--- src/main/java/org/chessgameai/Piece/Pawn.java
+++ src/main/java/org/chessgameai/Piece/Pawn.java
@@ -26,12 +26,15 @@ public class Pawn extends Piece {
     @Override
     public Collection<int[]> legalMoves() {
         List<int[]> legalMoves =  new ArrayList<>();
+        int y = this.piecePosition[0];//Pawn only vertically (Attack off)
+        int x = this.piecePosition[1];
+        int DIRECTION = this.pieceAlliance.getDirection();//+1 for WHITE and -1 for BLACK
         if (isFirstMove()) {
-            legalMoves.add(new int[]{this.piecePosition[0] + (this.pieceAlliance.getDirection() * 2), this.piecePosition[1]});
+            legalMoves.add(new int[]{y+(DIRECTION*2), x});
         }
-        if (this.piecePosition[0]+(this.pieceAlliance.getDirection()*1) < BoardUtils.NUM_TILES_PER_ROW
-                && this.piecePosition[0]+(this.pieceAlliance.getDirection()*1) > 0) {
-            legalMoves.add(new int[]{this.piecePosition[0] + (this.pieceAlliance.getDirection() * 1), this.piecePosition[1]});
+        if (y+(DIRECTION*1) < BoardUtils.NUM_TILES_PER_COLUMN
+                && y+(DIRECTION*1) > 0) {
+            legalMoves.add(new int[]{y+(DIRECTION*1), x});
         }
         return legalMoves;
     }
