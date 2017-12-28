6f030d0dc577bda801e3398f2ad99735d893ea00
Reda Nezzar <nezzar.reda@gmail.com>
2017-12-27 23:55:34 -0500

    Add the total time of a game in milliseconds (1 Hour)

--- src/main/java/org/chessgameai/board/BoardUtils.java
+++ src/main/java/org/chessgameai/board/BoardUtils.java
@@ -16,5 +16,6 @@ public enum BoardUtils {
     public static final int NUM_TILES_PER_ROW = 8;
     public static final int NUM_TILES_PER_COLUMN = 8;
     public static final int NUM_TILES = 64;
+    public static final int TIME_GAME = 1000*60*60;// an hour in ms
 
 }
