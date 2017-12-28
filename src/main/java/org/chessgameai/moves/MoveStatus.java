3a630aef653705c190909b97d0edcf3274ad7420
Reda Nezzar <nezzar.reda@gmail.com>
2017-12-28 00:15:31 -0500

    Move Stutus enum class for legal and illegal moves

---
+++ src/main/java/org/chessgameai/moves/MoveStatus.java
@@ -0,0 +1,17 @@
+package org.chessgameai.moves;
+
+/**
+ * Created by TheoWolf on 12/28/17.
+ */
+public enum  MoveStatus {
+
+    DONE {
+        @Override
+        public boolean isDone() { return true; }
+    },
+    IS_ILLEGAL_MOVE {
+        @Override
+        public boolean isDone() { return false; }
+    };
+    public abstract boolean isDone();
+}
